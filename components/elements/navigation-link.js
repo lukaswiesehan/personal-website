1d31d0b2ce414846ca8ffc111de5f5d8e25052d0
lukaswiesehan <lukas.wiesehan@icloud.com>
2021-06-29 15:05:38 +0200

    Update navigation-link.js for indicating current route

--- components/elements/navigation-link.js
+++ components/elements/navigation-link.js
@@ -1,10 +1,13 @@
 import Link from 'next/link'
+import {useRouter} from 'next/router'
 import {Emoji} from '@/components/elements/emoji'
 
 export const NavigationLink = ({href, anchor, emoji}) => {
+  const router = useRouter()
+
   return (
     <Link href={href}>
-      <a className="group flex items-center space-x-2 font-bold text-gray-700 dark:text-cool-gray-100">
+      <a className={`group flex items-center space-x-2 font-bold ${router.pathname === href ? 'text-teal-600 dark:text-teal-500' : 'text-gray-700 dark:text-cool-gray-100'}`}>
         <span className="border-b-2 border-transparent group-hover:border-teal-600 dark:group-hover:border-teal-500">{anchor}</span>
         <span className="
           opacity-0 transform -translate-x-3 scale-95
