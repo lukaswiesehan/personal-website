aa1bec0cc435d209ccebed4f3fd6190e1ba36aca
lukaswiesehan <lukas.wiesehan@icloud.com>
2021-06-29 15:01:22 +0200

    Fix submit-form.js

--- pages/api/submit-form.js
+++ pages/api/submit-form.js
@@ -4,7 +4,7 @@ export const SubmitForm = async (req, res) => {
   try {
     const {honeypot, name, phone} = req.body
     if(honeypot === '') {
-      const success = await sendNotification({message: `${name} bittet über das Website-Formular um Rückruf: <tel:${phone}|${phone}>`})
+      const success = await sendNotification({message: `${name} bittet über das Website-Formular um Rückruf: <tel:${phone.replace(' ', '')}|${phone}>`})
       res.status(success ? 200 : 500).json({
         success
       })
