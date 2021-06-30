78ec8b095f13cd1b1c4dda643b45de7c9231a476
lukaswiesehan <lukas.wiesehan@icloud.com>
2021-06-30 15:52:04 +0200

    Update card animation

--- components/layout/card.js
+++ components/layout/card.js
@@ -8,7 +8,7 @@ export const Card = ({className, children}) => {
   })
 
   const cardVariants = {
-    inView: {y: 0, opacity: 1, scale: 1, transition: {ease: 'linear'}},
+    inView: {y: 0, opacity: 1, scale: 1, transition: {ease: 'backIn'}},
     notInView: {y: 48, opacity: 0, scale:0.95}
   }
 
