630d58e6e838a432ad47fa6ebec524c21d878698
lukaswiesehan <lukas.wiesehan@icloud.com>
2021-06-30 19:12:01 +0200

    Add showcase.js placeholder

--- pages/showcase.js
+++ pages/showcase.js
@@ -1,9 +1,29 @@
 import {Container} from '@/components/layout/container'
+import {Title} from '@/components/layout/title'
+import {Card} from '@/components/layout/card'
+import {Heading} from '@/components/elements/heading'
+import {CardLink} from '@/components/elements/card-link'
 
 export const Showcase = () => {
   return (
     <Container>
-      <div>Showcase Content</div>
+      <Title
+        breadcrumbs={[{text: 'Showcase', emoji: 'call-me-hand'}]}
+        title={<h1><span className="text-teal-600 dark:text-teal-500">Aktuelle Projekte</span></h1>}
+        text={<p>Hier findest Du eine Auswahl meiner aktuellen Projekte und einige Details zur Umsetzung.</p>}
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <Card className="order-4 md:col-span-2">
+          <div className="p-7 pt-6">
+            <div className="space-y-4 mb-8">
+              <Heading>Schau' bei Gelengenheit nochmal rein</Heading>
+              <p className="w-3/4">Ich schreibe zu jedem der Projekte einen kleinen Artikel, um die technischen Details und Hintergründe möglichst ausführlich darzustellen. Hierfür brauche ich noch ein wenig Zeit.</p>
+              <p className="w-3/4">Du kannst mich in der Zwischenzeit kontaktieren, wenn Du genaueres zu meiner Arbeit wissen möchtest.</p>
+            </div>
+            <CardLink href="/contact" anchor="Kontakt aufnehmen"/>
+          </div>
+        </Card>
+      </div>
     </Container>
   )
 }
