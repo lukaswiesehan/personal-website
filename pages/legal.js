3b8ba3e1de704ba582ff8ae1b8c1e67c2073f630
lukaswiesehan <lukas.wiesehan@icloud.com>
2021-07-02 09:16:12 +0200

    Update legal.js for accessibility

--- pages/legal.js
+++ pages/legal.js
@@ -1,10 +1,8 @@
 import Mailto from 'react-protected-mailto'
 import {Container} from '@/components/layout/container'
 import {Title} from '@/components/layout/title'
-import {Card} from '@/components/layout/card'
 import {Heading} from '@/components/elements/heading'
 import {EmojiList} from '@/components/elements/emoji-list'
-import {CardLink} from '@/components/elements/card-link'
 
 export const Legal = () => {
   return (
@@ -14,28 +12,21 @@ export const Legal = () => {
         title={<h1><span className="text-teal-600 dark:text-teal-500">Impressum</span></h1>}
         text={''}
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <Card>
-          <div className="h-full p-7 pt-6 flex flex-col justify-between">
-            <div className="space-y-4 mb-2">
-              <Heading>Betreiber der Website</Heading>
-              <p><strong>Lukas Wiesehan</strong><br/>Design & Development</p>
-              <EmojiList elements={[
-                {emoji: 'house', text: <>Herrenstraße 30a<br/>21698 Harsefeld</>},
-                {emoji: 'e-mail', text: <Mailto email="mail@lukaswiesehan.de" obfuscatedHref="email"/>}
-              ]}/>
-            </div>
-          </div>
-        </Card>
-        <Card>
-          <div className="h-full p-7 pt-6 flex flex-col justify-between">
-            <div className="space-y-4 mb-2">
-              <Heading>Steuerliche Angaben</Heading>
-              <p><strong>Steuernummer: </strong>43/148/05020</p>
-              <p><strong>Umsatzsteuer-ID: </strong>DE341117007</p>
-            </div>
-          </div>
-        </Card>
+      <div className="space-y-12">
+        <div className="space-y-4">
+          <Heading>Betreiber der Website</Heading>
+          <p><strong>Lukas Wiesehan</strong><br/>Design & Development</p>
+          <EmojiList elements={[
+            {emoji: 'light-bulb', text: 'Einzelunternehmen'},
+            {emoji: 'house', text: <>Herrenstraße 30a<br/>21698 Harsefeld</>},
+            {emoji: 'e-mail', text: <Mailto email="mail@lukaswiesehan.de" obfuscatedHref="email"/>}
+          ]}/>
+        </div>
+        <div className="space-y-4">
+          <Heading>Steuerliche Angaben</Heading>
+          <p><strong>Steuernummer: </strong>43/148/05020</p>
+          <p><strong>Umsatzsteuer-ID: </strong>DE341117007</p>
+        </div>
       </div>
     </Container>
   )
