84574472950e1af18bf78767e94396f96a446edd
lukaswiesehan <lukas.wiesehan@icloud.com>
2021-07-08 15:06:38 +0200

    Add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="preload" href="/fonts/lato-latin-regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
           <link rel="preload" href="/fonts/lato-latin-black.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
+          <script async defer data-website-id="4bcb1c40-89a7-4c27-a9a5-b14a3242d0bf" src="https://analytics.lukaswiesehan.de/umami.js"></script>
         </Head>  
         <body>
           <Main/>
